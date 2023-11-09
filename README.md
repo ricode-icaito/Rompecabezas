985798c3cc36963cb52070c47bdd9184d893d3dd
Ricardo <146256152+ricode-icaito@users.noreply.github.com>
2023-11-08 19:43:43 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# Rompecabezas
-Rompecabezas de un tigre
+## Java Script Puzzle 3x3
+<p align="center"><img src="https://github.com/AprendeAProgramar/JS_Puzzle/blob/master/puzzle.jpg"></p>
+
+<p align="center">
+<a href="https://www.youtube.com/watch?v=puR1o3Jww_8"><img src="https://img.youtube.com/vi/puR1o3Jww_8/0.jpg" alt="Video con la programación del ejercicio"></a>
+</p>
+
+## Descripción del proyecto:
+
+Ejercicio que pone en práctica HTML, CSS y Java Script para la creación de una página Web que permite ver un puzzle 3x3 y solucionarlo:
+
+- Maquetación básica HTML con una tabla de 3x3
+- Uso de estilos CSS para resaltar la casilla seleccionada
+- Prácticar conceptos básicos de Java Script como declaración y uso de variables, arrays e instrucciones de control básicas
+- Control y respuesta del evento click
+
+## License
+
+Este proyecto es open-source publicado con fines educativos, bajo [MIT license](https://opensource.org/licenses/MIT).
---
+++ script.js
@@ -0,0 +1,111 @@
+function desordenar() {
+	piezas.sort(function() {return Math.random() - 0.5});
+	console.log(piezas);
+}
+function desmarcarTodas() {
+  
+//Elimina el borde de todas las casillas (aunque no lo tuviesen)
+	for (var i = 0;i <= 8;i ++) {
+		document.getElementById("img_" + i).style.border = "solid 2px white";
+	}
+}
+
+//Esta función se le llama cuando se hace click en una casilla:
+function seleccionar(casilla) {
+  
+//Contabilizo el click sobre una casilla:
+	num_click = num_click + 1;
+	console.log("Click num: " + num_click);
+
+//Si es el primero click:
+	if (num_click == 1) {
+    
+//Memorizar la casilla donde se ha hecho este primer click:
+		casilla_click_primero = casilla;
+		desmarcarTodas();
+		document.getElementById("img_" + casilla).style.border = "solid 2px red";
+		console.log("Memorizo la casilla del primer click: " + casilla_click_primero);
+	}
+
+	if (num_click == 2) {
+    
+//En este caso, este segundo click, no me interesa mantenerlo, con lo cual la variable puede ser local
+		var casilla_click_segundo = casilla;
+
+//Pintar en la consola como esta el array antes del intercambio:
+		console.log(piezas);
+
+//Intercambiar los valores en las posiciones del array:
+		var contenido = piezas[casilla_click_primero];
+    
+//Ahora ya puedo cambiar el contenido de esa casilla:
+		piezas[casilla_click_primero] = 
+    piezas[casilla_click_segundo];
+    
+//Por último, pongo en la casilla del segundo click, el contenido que había en la primera:
+		piezas[casilla_click_segundo] = contenido;
+
+//Pintar en la consola como ha quedado el array:
+		console.log(piezas);
+
+//Volver a poner el contador de clicks a 0:
+		num_click = 0;
+
+//Quitar el borde que estaba marcado:
+		desmarcarTodas();
+
+//Aquí deberíamos refrescar el panel:
+		refrescarPuzzle();
+
+//Comprobar si el puzzle ya es correcto:
+		var correcto = comprobarPuzzleFinalizado();
+    
+//Al salir del bucle la variable correcto me dice true si esta completo y false si no:
+		if (correcto == true) {
+      
+//Mostramos el alert mediante un breve temporizador, para que de tiempo a refrescar el puzzle y verlo completo:
+			setTimeout(function(){
+			 	alert("Puzzle finalizado");
+			}, 300);
+		}
+	}
+}
+
+function comprobarPuzzleFinalizado() {
+  
+//Recorrer todo el array piezas y ver el contenido de cada casilla se corresponde con su posición:
+	var correcto = true;
+	for (var i = 0;i <= 8;i ++) {
+		if (piezas[i] != i) {
+			correcto = false;
+		}
+	}
+	return correcto;
+}
+
+function refrescarPuzzle() {
+	for (var casilla = 0;casilla <= 8;casilla ++) {
+    
+//Obtengo el número de la imagen que hay en el array en la casilla i:
+		var imagen = piezas[casilla];
+    
+//Pinto en la casilla i, la imagen imagen:
+		document.getElementById("img_" + casilla).src = "imagenes/img_" + imagen + ".jpg";
+	}
+}
+
+//Comienzo el programa:
+var piezas = [0,1,2,3,4,5,6,7,8];
+
+//Variable para distinguir si es el primer o segundo click:
+var num_click = 0;
+
+//Variable para saber en que casilla se hizo el primer click:
+var casilla_click_primero;
+
+//Llamo a la función desordenar:
+desordenar();
+
+//LLamo a la función que refresca el panel con el puzzle:
+refrescarPuzzle();
+desmarcarTodas();
